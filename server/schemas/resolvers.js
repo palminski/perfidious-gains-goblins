6a941dd7fcf9259f6f6cb3aa12927d5e6d3a98cf
palminski <williambolls98@gmail.com>
2022-12-06 14:24:04 -0800

    added ability to add mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -14,7 +14,16 @@ const resolvers = {
         deleteUser: async(parent, {_id}) => {
             const user = await User.deleteOne({_id: _id});
             return user;
-        }
+        },
+        //This will be updated with login context in the future.
+        addExcersize: async(parent, {userId, excersize, ammount, units, reps, sets}) => {
+            const updatedUser = await User.findOneAndUpdate(
+                {_id: userId},
+                {$push: {excersizes: {excersize: excersize, ammount:ammount, units:units, reps:reps, sets:sets}}},
+                {new:true, runValidators:true}
+            );
+            return updatedUser;
+        },
     }
 };
 
